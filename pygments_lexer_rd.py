ea89b8a07620575d3213bee6b904548573ac1924
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-08-14 22:23:40 -0400

    simpler version that words

--- pygments_lexer_rd.py
+++ pygments_lexer_rd.py
@@ -1,84 +1,43 @@
 # -*- coding: utf-8 -*-
-""" Pygments lexer for Rd files
+""" Pygments lexer for R documentation (Rd) files
 
     :copyright: Copyright 2012 Jeffrey B. Arnold
     :license: BSD, see LICENSE for details.
 """
-from pygments.lexer import RegexLexer, include, bygroups, using
-from pygments.lexers.math import SLexer
+from pygments.lexer import RegexLexer, include, bygroups
 from pygments.token import Comment, Punctuation, Keyword, Name, \
-     Operator, Number, Text, String
+     Operator, Number, Text
+
+__all__ = ['RdLexer']
 
 class RdLexer(RegexLexer):
-    """
-    Lexer for the Rd (R documentation) format
-    """
+    """ Pygments Lexer for R documentation (Rd) files
 
+    This is a very minimal implementation, highlighting little more
+    than the macros. A description of Rd syntax is found in `Writing R
+    Extensions <http://cran.r-project.org/doc/manuals/R-exts.html>`_
+    and `Parsing Rd files <developer.r-project.org/parseRd.pdf>`_.
+
+    """
     name = 'Rd'
     aliases = ['rd']
     filenames = ['*.Rd']
 
-    # macros with no arguments
-    CONSTANTS = ['cr', 'dots', 'ldots', 'R', 'tab']
-
-    # Macros with R text
-    RLIKE = ['code', 'dontshow', 'donttest', 'testonly',
-             'examples', 'usage']
-    # Verbatim and 1 argument
-    VERBATIM1 = ['dontrun', 'env', 'kbd', 'option',
-                 'out', 'preformatted', 'samp', 'special',
-                 'url', 'verb', 'deqn', 'eqn',
-                 'alias', 'Rdversion', 'synopsis', 'RdOpts', 'Sexpr', 
-                 'href' # 2nd arg of href will be formatted normally
-                 ]
-    # Verbatim and 2 arguments
-    VERBATIM2 = ['newcommand', 'renewcommand', 'deqn', 'eqn']
-    # Can be included in strings in R-like text
-    SPECIAL = ['link', 'var']
-
     tokens = {
-        'brackets': [
-            (r'(?<!\\)\{', String, '#push')
-            (r'(?<!\\)\}', String, '#pop')
-            ],
-        'comment': [
-            (r'%.*?\n', Comment.Single),  
-            ],
-        'macro':[
-            (r'\\([a-zA-Z]+\b)', Keyword),            
-            ],
-        'string_macros':[
-            (r'\\%s\b)' % r'|'.join(SPECIAL), Keyword),
-            ],
-        # latexlike
-        'root': [
-            # Pre-processor
-            (r'^\#(ifn?def|endif).*$', Comment.Preproc, 'preproc'),
-            # Other macros
-            (r'[{}]', Operator),
-            (r'.', Text)
-        ],
-        'options': [
-            ],
-        'verbatim': [
-            include('comment')
-            include('brackets')
-            (r'.', String)
-            ],
-        'rlike': [
-            include('comment')
-            include('macro')
-            (r'\'', String, 'string_squote'),
-            (r'\"', String, 'string_dquote'),
-            include('brackets')
-            (r'.', String)
-            ],
-        'string_squote': [
-            (r'[^\']*\'', String, '#pop'),
-        ],
-        'string_dquote': [
-            (r'[^\"]*\"', String, '#pop'),
-        ],
+        'root' : [
+            # catch escaped brackets and percent sign
+            (r'\\[{}%]', String.Escape),
+            # comments
+            (r'%.*$', Comment),
+            # special macros with no arguments
+            (r'\\(?:cr|l?dots|R|tab)\b', Keyword.Constant),
+            # macros
+            (r'\\[a-zA-Z]+\b', Keyword),
+            # special preprocessor macros
+            (r'^#(?:ifn?def|endif).*\b', Comment.Preproc),
+            # Non escaped brackets
+            (r'[{}\[\]]', Punctuation),
+            ]
+        }
 
-    }
 
